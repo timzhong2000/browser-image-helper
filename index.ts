9088b2bd2c1b061c6052b1af7015a1c7d1c6e03e
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2022-10-11 22:57:50 +0800

    perf: bypass toImageBitmap

--- index.ts
+++ index.ts
@@ -1,60 +1,81 @@
 export class ImageHelper {
-  private canvas = document.createElement("canvas");
-  constructor(private image?: ImageBitmapSource) {}
+  /**
+   * it can boost the performance if the image to be processed is mutable
+   */
+  private sharedCanvas = document.createElement("canvas");
+  private image: ImageBitmapSource;
 
-  setImage(image: ImageBitmapSource) {
-    this.image = image;
+  constructor(image?: ImageBitmapSource, private mutable: boolean = false) {
+    image && this.setImage(image);
   }
 
-  /**
-   * create OffscreenCanvas from ImageBitmap. your browser should support OffscreenCanvas
-   */
-  async toOffscreenCanvas() {
-    const imageBitmap = await this.toImageBitmap();
-    const { width, height } = imageBitmap;
-    if (!window.OffscreenCanvas)
-      throw new Error("browser does not support OffscreenCanvas");
-    const offscreenCanvas = new OffscreenCanvas(width, height);
-    const ctx = offscreenCanvas.getContext("2d", {
+  static getContextInCPURam(canvas: HTMLCanvasElement | OffscreenCanvas) {
+    const context = canvas.getContext("2d", {
       alpha: false,
       willReadFrequently: true,
       desynchronized: false,
     });
-    if (!ctx) throw new Error("get RenderingContext2D failed");
-    ctx.drawImage(imageBitmap, 0, 0);
-    return { canvas: offscreenCanvas, context: ctx };
-  }
-
-  /**
-   * @param copy false: use a shared canvas
-   * @returns
-   */
-  async toOnscreenCanvas(copy = true) {
-    const imageBitmap = await this.toImageBitmap();
-    const { width, height } = imageBitmap;
-    const canvas = copy ? document.createElement("canvas") : this.canvas;
-    canvas.width = width;
-    canvas.height = height;
-    const ctx = canvas.getContext("2d", {
-      alpha: false,
-      willReadFrequently: true,
-      desynchronized: false,
-    });
-    if (!ctx) throw new Error("get RenderingContext2D failed");
-    ctx.drawImage(imageBitmap, 0, 0);
-    return { canvas, context: ctx };
+    if (!context) throw new Error("get RenderingContext2D failed");
+    return context;
   }
 
   /**
    *
-   * @param fileName
+   * @param image Do Not support Blob
+   * @param context
+   */
+  static drawOnContext(
+    image: ImageBitmapSource,
+    context: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D,
+    sx: number,
+    sy: number,
+    sw: number,
+    sh: number
+  ) {
+    if (image instanceof Blob) {
+      throw new Error("drawOnContext does not support Blob");
+    } else if (image instanceof ImageData) {
+      context.putImageData(image, 0, 0, sx, sy, sw, sh);
+    } else {
+      context.drawImage(image, sx, sy, sw, sh, 0, 0, sw, sh);
+    }
+  }
+
+  setImage(image: ImageBitmapSource) {
+    this.image = image;
+    return this;
+  }
+
+  /**
+   * create OffscreenCanvas from ImageBitmap. your browser should support OffscreenCanvas!
+   */
+  async toOffscreenCanvas(sx: number, sy: number, sw: number, sh: number) {
+    if (!window.OffscreenCanvas)
+      throw new Error("browser does not support OffscreenCanvas");
+    const { width, height } = await this.getResolution();
+    const offscreenCanvas = new OffscreenCanvas(width, height);
+    const context = ImageHelper.getContextInCPURam(offscreenCanvas);
+    ImageHelper.drawOnContext(this.image, context, sx, sy, sw, sh);
+    return { canvas: offscreenCanvas, context: context };
+  }
+
+  async toOnscreenCanvas(sx: number, sy: number, sw: number, sh: number) {
+    // create canvas and draw
+    const canvas = this.mutable
+      ? this.sharedCanvas
+      : document.createElement("canvas");
+    canvas.width = sw;
+    canvas.height = sh;
+    const context = ImageHelper.getContextInCPURam(canvas);
+    ImageHelper.drawOnContext(this.image, context, sx, sy, sw, sh);
+
+    return { canvas, context };
+  }
+
+  /**
+   * @param fileName length <= 255
    * @param fileProperty lastModified field
    * @param options image format and quality
-   * @param sx
-   * @param sy
-   * @param sw
-   * @param sh
-   * @returns
    */
   async toFile(
     fileName: string,
@@ -70,13 +91,7 @@ export class ImageHelper {
   }
 
   /**
-   *
    * @param options image format and quality
-   * @param sx
-   * @param sy
-   * @param sw
-   * @param sh
-   * @returns
    */
   async toBlob(
     options?: { type?: string | undefined; quality?: number | undefined },
@@ -86,26 +101,31 @@ export class ImageHelper {
     sh?: number
   ): Promise<Blob> {
     if (this.image instanceof Blob) return this.image;
-    const imageBitmap = await this.toImageBitmap(sx, sy, sw, sh);
-    const { canvas } = await this.toOnscreenCanvas();
+    const { width, height } = await this.getResolution();
+    const { canvas } = await this.toOnscreenCanvas(
+      sx ?? 0,
+      sy ?? 0,
+      sw ?? width,
+      sh ?? height
+    );
     return new Promise((resolve, reject) =>
-      canvas.toBlob((blob) => (blob ? resolve(blob) : reject()))
+      canvas.toBlob(
+        (blob) => (blob ? resolve(blob) : reject()),
+        options?.type,
+        options?.quality
+      )
     );
   }
 
-  /**
-   *
-   * @param sx
-   * @param sy
-   * @param sw
-   * @param sh
-   * @returns
-   */
   async toImageData(sx?: number, sy?: number, sw?: number, sh?: number) {
     if (this.image instanceof ImageData) return this.image;
-    const imageBitmap = await this.toImageBitmap(sx, sy, sw, sh);
-    const { width, height } = imageBitmap;
-    const { context } = await this.toOnscreenCanvas();
+    const { width, height } = await this.getResolution();
+    const { context } = await this.toOnscreenCanvas(
+      sx ?? 0,
+      sy ?? 0,
+      sw ?? width,
+      sh ?? height
+    );
     const blob = context.getImageData(
       sx ?? 0,
       sy ?? 0,
@@ -135,5 +155,12 @@ export class ImageHelper {
     } else {
       return await createImageBitmap(this.image);
     }
+  }
+
+  async getResolution() {
+    const imageBitmap = await this.toImageBitmap();
+    const { width, height } = imageBitmap;
+    imageBitmap.close();
+    return { width, height };
   }
 }
